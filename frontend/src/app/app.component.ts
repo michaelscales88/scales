ea2709e07efca5d5299d6d460ac0820507b8187a
Michael Scales <mscales@mindwireless.com>
2018-01-22 13:07:51 -0600

    add datatables example to integrate

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { DataTablesModule } from 'angular-datatables';
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+}
